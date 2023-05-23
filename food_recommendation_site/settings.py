82269cec712f384035e1a9d0feb3989602cf0d45
Oleksandr Dyriv <dyriv.alex@gmail.com>
2023-05-23 22:19:07 +0300

    testing

--- food_recommendation_site/settings.py
+++ food_recommendation_site/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'food_recommendation_site.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -98,7 +98,7 @@ DATABASES = {
         'NAME': BASE_DIR / 'db/db.sqlite3',
     }
 }
-'''
+
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
