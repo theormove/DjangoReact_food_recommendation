e8d106cd1a0fb158d3095fb6835b32aa8fd326ef
Oleksandr Dyriv <dyriv.alex@gmail.com>
2023-06-04 21:06:25 +0300

    use psql

--- food_recommendation_site/settings.py
+++ food_recommendation_site/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'food_recommendation_site.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-'''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -99,6 +99,7 @@ DATABASES = {
     }
 }
 
+'''
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
